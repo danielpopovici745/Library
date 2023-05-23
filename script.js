38b59a5bba211f961a6816ce034f7b529a6155d0
Daniel Popovici <dpopovici601@gmail.com>
2023-05-23 13:02:23 -0400

    added comments to describe code, fixed some small eslint problems

--- script.js
+++ script.js
@@ -9,26 +9,31 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-Book.prototype.info = function () {
+Book.prototype.info = function info() {
   return `${this.title}, ${this.author}, ${this.pages}, ${this.read}`;
 };
 
 function addBookToLibrary() {
   const addBookFormInputs = document.querySelectorAll(".addBookForm input");
+  // converts NodeList to array with book data
+  // ['title','author','pages','completed']
   const bookFormArrayValues = Array.from(addBookFormInputs).map(
     (input) => input.value
   );
   if (
+    // checks to make sure book values are not empty,undefined or null
     bookFormArrayValues.every(
-      (value) => value != "" && value != undefined && value != null
+      (value) => value !== "" && value !== undefined && value !== null
     )
   ) {
+    // create an object with the values from bookFormArrayValues
     const book = new Book(
       bookFormArrayValues[0],
       bookFormArrayValues[1],
       bookFormArrayValues[2],
       bookFormArrayValues[3]
     );
+    // send book object to users library array for storage and then will be added to the table for the user to view
     myLibrary.push(book);
     addBookToTable();
   }
